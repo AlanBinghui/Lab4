33927dc0ee5bb33f091a14f7b979436dd1d157d1
KrazyOranGe <KrazyOranGe@10.0.8.209>
2017-09-27 16:26:25 -0500

    Signed-off-by: KrazyOranGe <KrazyOranGe@10.0.8.209>

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -10,17 +10,25 @@
 
 public class ReclamationProject
 {
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
+    static String doIt (String a,String b) {
+        if (a.length() > b.length()) {
+            String c = a; // TODO: set b to a
+            a=b;
+            b=c;
+        }
+        String r = ((a.equals(b)) ? "" : "");
         /*
          * For loop with i
          */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
+        for (int i = 0; i < a.length(); i++) {
+            for (int j = a.length() - i; j > 0; j--) {
                 for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+                   if ( (a.regionMatches(i, b, k, j) && j > r.length())) {
+                       r = a.substring(i,i + j) ;
+                   }
+                      }
+                }
+        }
+        return r;
+        }
 }
